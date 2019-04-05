c36119ed51485a31f61ab62a6f4cc46a466755c9
kbowater57 <49311506+kbowater57@users.noreply.github.com>
2019-04-05 18:14:29 +0100

    basic functions complete
    move, inv, stat, look, help

--- main.py
+++ main.py
@@ -21,35 +21,53 @@ def move(character,room):
         return character
     
     proposed_location = character["location"] + dir_vector
-    if proposed_location.real not in list(range(int(room.real))):
-        print("You just walked into a wall. Ouch! You take 1 damage.")
-        character["health"] -= 1
-        return character
-    elif int(proposed_location.imag) < 0 or int(abs(proposed_location.imag)) not in list(range(int(abs(room.imag)))):
+    
+    if (proposed_location.real not in list(range(int(room.real))) or
+        int(proposed_location.imag) < 0 or int(abs(proposed_location.imag)) 
+        not in list(range(int(abs(room.imag))))):
+        
         print("You just walked into a wall. Ouch! You take 1 damage.")
         character["health"] -= 1
         return character
     else:
-        display_location(proposed_location,character["roomno"])
         character["location"] = proposed_location
+        display_location(character)
         return character
 
-def display_location(location, roomno):
-    print("You are at co-ordinate (" + str(int(location.imag)) + ", " + str(int(location.real)) + ") in room " + str(roomno) + "." )
+def display_location(character):
+    print(("You are at co-ordinate (" + str(int(character["location"].imag)) + ", " + 
+        str(int(character["location"].real)) + ") in room " + str(character["roomno"]) + ". Room " +
+        str(character["roomno"]) + " is " + str(int(character["roomsize"].real)) 
+        + " wide by " + str(int(character["roomsize"].imag)) + " deep."))
     
 def stat(character):
-    display_location(character["location"])
-    print("You have " + character["health"] + " hitpoints remaining.")
+    display_location(character)
+    print("You have " + str(character["health"]) + " hitpoints remaining.")
+    
+def help_text():
+    help = open("help.txt","r")
+    for line in help:
+        print(line)
+    help.close()
+    
+def room_desc(character):
+    room_text = "room" + str(character["roomno"]) + ".txt"
+    text = open(room_text,"r")
+    for line in text:
+        print(line)
+    text.close()
     
     
 room1 = 4 + 5j
-character = {"location":0+0j,"health":2,"roomno":1, "roomsize":room1}
+character = {"location":0+0j,"health":2,"roomno":1, "roomsize":room1, "inv":{"sword":10}}
 
 #print(current_location["x"] in list(range(room1["length"])))
 
-intro_text = open("intro.txt","r")
-for line in intro_text:
-    print(line)
+room_desc(character)
+help_text()
+
+    
+    
 quest_complete = 0
 while quest_complete == 0:
     if character["health"] <=0:
@@ -62,7 +80,15 @@ while quest_complete == 0:
         print("Oh no! You died. Better luck next time!")
         break
     elif requested_action == "stat":
-        stat
+        stat(character)
+    elif requested_action == "disp":
+        display_location(character)
+    elif requested_action == "help":
+        help_text()
+    elif requested_action == "look":
+        room_desc(character)
+    elif requested_action == "inv":
+        print(character["inv"])
     else:
         print("I don't know how to " + requested_action + ".")
     
