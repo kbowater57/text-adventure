ab2d12fc94af60b2e50e0a0e42744510860bd140
kbowater57 <49311506+kbowater57@users.noreply.github.com>
2019-04-06 23:57:12 +0100

    Added win condition
    Added third room, with chest. Landing on the chest square ends the game.
    Things still to add: ability to take torch from first room, orc in corridor, looting key from orc's body, using key on chest to open it.

--- main.py
+++ main.py
@@ -26,13 +26,18 @@ def move(direction,character,room_point_set):
         print("You just walked into a wall. Ouch! You take 1 damage.")
         character["health"] -= 1
         return character
-    elif proposed_location == -2+2j and "torch" not in character["inv"]:
-        print("You were eaten by a grue. Better bring a torch next time.")
-        character["health"] = 0
+    elif proposed_location == -10 + 7j:
+        print("You approach the chest, and throw it open eagerly. A huge bounty of gold coins lies within the chest. Congratulations! You have won the game!")
+        character["quest_complete"] = 1
         return character
     else:
         character["location"] = proposed_location
         return character
+    """elif proposed_location == -2+2j and "torch" not in character["inv"]:
+        print("You were eaten by a grue. Better bring a torch next time.")
+        character["health"] = 0
+        return character"""
+    
 
 def display_location(character):
     print(("You are at co-ordinate (" + str(int(character["location"].real)) + ", " + 
@@ -82,15 +87,20 @@ room0_list=add_room(room0_dict,room0_list)
 room1_dict = {"corner_ne": (-1+2j),"corner_sw": (-6+2j)}
 room1_list = list()
 room1_list=add_room(room1_dict,room1_list)
+room2_dict = {"corner_ne": (-7+8j),"corner_sw": (-13+1j)}
+room2_list = list()
+room2_list=add_room(room2_dict,room2_list)
 
-room_points_dict = {"room0":room0_list,"room1":room1_list}
+
+room_points_dict = {"room0":room0_list,"room1":room1_list,"room2":room2_list}
 room_point_list=list()
 
     
 room_point_list = add_room(room0_dict,room_point_list)
 room_point_list = add_room(room1_dict,room_point_list)
+room_point_list = add_room(room2_dict,room_point_list)
 
-character = {"location":0+0j,"health":2,"roomno":0, "inv":{"sword":10}}
+character = {"location":0+0j,"health":2,"roomno":0, "inv":{"sword":10}, "quest_complete":0}
 
 
 room_desc(character)
@@ -98,8 +108,8 @@ help_text()
 
     
     
-quest_complete = 0
-while quest_complete == 0:
+
+while character["quest_complete"] == 0:
     if character["health"] <=0:
         print("Oh no! You died. Better luck next time!")
         break
@@ -111,6 +121,14 @@ while quest_complete == 0:
     requested_action = input("What would you like to do? \n")
     if requested_action[0:5] == "move ":
         character = move(requested_action[5:],character,room_point_list)
+    elif requested_action[0] == "n" and len(requested_action) == 1:
+        character = move("n",character,room_point_list)
+    elif requested_action[0] == "e" and len(requested_action) == 1:
+        character = move("e",character,room_point_list)
+    elif requested_action[0] == "s" and len(requested_action) == 1:
+        character = move("s",character,room_point_list)    
+    elif requested_action[0] == "w" and len(requested_action) == 1:
+        character = move("w",character,room_point_list)
     elif requested_action == "die":
         print("Oh no! You died. Better luck next time!")
         break
